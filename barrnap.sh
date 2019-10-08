828f95caa0b36834580465552b130a1b2715e1ff
Robert W Murdoch <robert.waller.murdoch@gmail.com>
2019-10-08 12:29:53 -0400

    Update barrnap.sh

--- barrnap.sh
+++ barrnap.sh
@@ -1,3 +1,4 @@
 # A simple rapid command for harvesting rRNA genes from a genome fasta
+# I run this from a dedicated conda environment which has bedtools and hmmer
 
 barrnap -t 8 -o output.rRNA.fa YOURGENOME > RNA.gff
